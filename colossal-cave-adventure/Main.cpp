4050666b71de136f34e3603a16ac513dbb22efae
Leongito Nicolò <leongito.nicolo@gmail.com>
2026-06-25 21:36:13 +0200

    main

--- colossal-cave-adventure/Main.cpp
+++ colossal-cave-adventure/Main.cpp
@@ -2,20 +2,10 @@
 #include <string>
 #include <iostream>
 
-#include "Player.h"
-#include "Room.h"
-#include "CommandParser.h"
 #include "GameManager.h"
-
-using namespace std;
 
 int main()
 {
-	//Item keys = Item(0, "Key", "A set of keys");			//KeyItem
-	//Item lantern = Item(1, "Lantern", "A lantern");			//ToolItem
-	//Item bottle = Item(2, "Bottle", "A bottle of water");	//PuzzleItem
-	//Item food = Item(3, "Food", "Some food");				//ToolItem
-
 	GameManager game;
 	game.StartGame();
 
